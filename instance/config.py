d62e5316453e3ab65f53a13b1db8059ee6254b14
Liam Ellis <liamhellis@gmail.com>
2017-06-07 22:02:12 -0400

    first commit, project structure, config files

---
+++ instance/config.py
@@ -0,0 +1,35 @@
+import os
+
+class Config(object):
+    #Parent Configuration Class
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+
+class DevelopmentConfig(Config):
+    #Configurations for Development
+    DEBUG = True
+
+class TestingConfig(Config):
+    #Configurations for Testing, with a separate database
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://test_db'
+    DEBUG = True
+
+class StagingConfig(Config):
+    #Configurations for staging
+    DEBUG = True
+
+class ProductionConfig(Config):
+    #Configurations for Production
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'staging': StagingConfig,
+    'production': ProductionConfig
+}
